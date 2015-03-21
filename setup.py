2f43fef32f172fe63fafff8764c9c0da3b0beebf
Lucas Hoffmann <l-m-h@web.de>
2015-03-21 12:44:31 +0100

    Fix errors and improvements in setup.py.
    Add requirements and install all packages.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='s5',
     description='Secure Self hosted Synchronization and Sharing Service (S5)',
-    author='Matthias Riegel',
+    author='wonko@hanstool.org',
     version='0.0',
-    packages=['s5'],
-    entry_points={'console_scripts': ['s5server = s5.server', 's5 = s5.client']}
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['s5server = s5.server', 's5 = s5.client']
+    },
+    install_requires=['pycrypto'],
 )
